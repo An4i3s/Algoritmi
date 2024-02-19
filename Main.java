f4c958492f501fd6d02c94a7be245cfa53762e94
Anais <anais.casamento@itsrizzoli.it>
2024-02-19 12:07:40 +0100

    aggiunto BubbleSort

--- Main.java
+++ Main.java
@@ -3,21 +3,25 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        InsertionSort iSort = new InsertionSort();
+       // InsertionSort iSort = new InsertionSort();
         int[] ints = {1,5,2,300,87, 04};
-        int[] sortedInts = iSort.insertionSort(ints);
-        System.out.println(Arrays.toString(sortedInts));
-        iSort.reversedInsertionSort(ints);
-        iSort.calculateExeTime(ints);
+        // int[] sortedInts = iSort.insertionSort(ints);
+        // System.out.println(Arrays.toString(sortedInts));
+        // iSort.reversedInsertionSort(ints);
+        // iSort.calculateExeTime(ints);
 
-        System.out.println("*".repeat(30));
+        // System.out.println("*".repeat(30));
 
-        MergeSort mSort = new MergeSort();
-        int n = ints.length;
+        // MergeSort mSort = new MergeSort();
+        // int n = ints.length;
         //mSort.mergeSort(sortedInts, n);
 
-        System.out.println("*".repeat(30));
-        SelectionSort sSort = new SelectionSort();
-        sSort.selectionSort(ints);
+        // System.out.println("*".repeat(30));
+        // SelectionSort sSort = new SelectionSort();
+        // sSort.selectionSort(ints);
+
+        BubbleSort bSort = new BubbleSort();
+        bSort.bubbleSort(ints);
+        
     }
 }
